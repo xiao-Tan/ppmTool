495d7431edc96e38dfc8e3de4dd872482e4e6806
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-03-07 00:09:57 -0800

    service and controller

---
+++ javaFullStack/src/main/java/io/xiaotan/ppmtool/services/ProjectService.java
@@ -0,0 +1,21 @@
+package io.xiaotan.ppmtool.services;
+
+import io.xiaotan.ppmtool.domain.Project;
+import io.xiaotan.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired // ?????????????????
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        //Logic
+
+        return projectRepository.save(project);
+    }
+
+}
