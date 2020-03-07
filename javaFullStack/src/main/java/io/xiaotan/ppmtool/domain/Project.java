33a9471997e10c7995179a58dace58a8fa0d9195
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-03-06 23:29:47 -0800

    project object and repo

--- javaFullStack/src/main/java/io/xiaotan/ppmtool/domain/Project.java
+++ javaFullStack/src/main/java/io/xiaotan/ppmtool/domain/Project.java
@@ -1,4 +1,101 @@
 package io.xiaotan.ppmtool.domain;
 
+
+import javax.persistence.*;
+import java.util.Date;
+
+//实体类
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //主键自动生成策略
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_At;
+    private Date updated_At;
+
+    //Constructor
+    public Project() {
+    }
+
+    //all attributes getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Date getUpdated_At() {
+        return updated_At;
+    }
+
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At = updated_At;
+    }
+
+    @PrePersist
+    protected void onCreated(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdated(){
+        this.updated_At = new Date();
+    }
+
 }
---
+++ javaFullStack/src/main/java/io/xiaotan/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.xiaotan.ppmtool.repositories;
+
+import io.xiaotan.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
