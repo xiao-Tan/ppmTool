cae40d1c21a3984c77ab9287e84b877af1304cb1
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-03-07 00:51:57 -0800

    validation part1

--- javaFullStack/src/main/java/io/xiaotan/ppmtool/web/ProjectController.java
+++ javaFullStack/src/main/java/io/xiaotan/ppmtool/web/ProjectController.java
@@ -5,10 +5,13 @@ import io.xiaotan.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,8 +21,13 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createdNewProject(@RequestBody Project project){
-        Project project1 = projectService.saveOrUpdateProject(project); //project1 doing??????
+    public ResponseEntity<?> createdNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
+
+        Project project1 = projectService.saveOrUpdateProject(project); //add and save to database. project1 doing??????
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
 
