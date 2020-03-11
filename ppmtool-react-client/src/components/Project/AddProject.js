1cdf63fc08dc36d99a52467a617ec0d33f24c9a4
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-03-10 20:59:31 -0700

    createProject

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectAction";
 
 //change class to className
 //check name attribute input field(确保form里的name与Project Object一致)
@@ -41,8 +44,7 @@ class AddProject extends Component {
       start_date: this.state.start_date,
       end_date: this.state.end_date
     };
-
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -116,4 +118,9 @@ class AddProject extends Component {
     );
   }
 }
-export default AddProject;
+
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired
+};
+
+export default connect(null, { createProject })(AddProject);
