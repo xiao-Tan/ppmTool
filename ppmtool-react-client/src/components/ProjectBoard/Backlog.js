abffe9e34b51b68613a37184558ff4088007f7f6
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-03-18 21:03:55 -0700

    folder structure

---
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class  extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default Backlog;
---
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AddProjectTask extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default AddProjectTask;
