741a450d7e6ffe8847c32069bba8ec291648ae86
谭笑 <tanxiao@tanxiaodeMacBook-Pro.local>
2020-04-04 18:30:58 -0700

    secureRoute

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -18,6 +18,7 @@ import jwt_decode from "jwt-decode";
 import setJwtToken from "./securityutil/setJwtToken";
 import { SET_CURRENT_USER } from "./actions/types";
 import { logout } from "./actions/securityAction";
+import SecureRoute from "./securityutil/SecureRoute";
 
 //从localStorage里提取token，如果存在（即没过期）重新写入state
 const jwtToken = localStorage.jwtToken;
@@ -55,16 +56,30 @@ function App() {
             //personal route
           }
 
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-          <Route exact path="/updateProject/:id" component={UpdateProject} />
-          <Route exact path="/projectBoard/:id" component={ProjectBoard} />
-          <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
-          <Route
-            exact
-            path="/updateProjectTask/:id/:sequence"
-            component={UpdateProjectTask}
-          />
+          <Switch>
+            <SecureRoute exact path="/dashboard" component={Dashboard} />
+            <SecureRoute exact path="/addProject" component={AddProject} />
+            <SecureRoute
+              exact
+              path="/updateProject/:id"
+              component={UpdateProject}
+            />
+            <SecureRoute
+              exact
+              path="/projectBoard/:id"
+              component={ProjectBoard}
+            />
+            <SecureRoute
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            />
+            <SecureRoute
+              exact
+              path="/updateProjectTask/:id/:sequence"
+              component={UpdateProjectTask}
+            />
+          </Switch>
         </div>
       </Router>
     </Provider>
